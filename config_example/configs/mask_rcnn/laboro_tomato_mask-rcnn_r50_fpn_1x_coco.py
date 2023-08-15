3a24871322118a01cf89c7878db5fd32a0823022
Dewen Xu <51310971+XuDewen@users.noreply.github.com>
2023-08-15 11:04:51 +0900

    upload example files

---
+++ config_example/configs/mask_rcnn/laboro_tomato_mask-rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/laboro_tomato_mask-rcnn_r50_fpn.py',
+    '../_base_/datasets/laboro_tomato_coco_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
